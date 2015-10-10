0ca16a8f0b7f2690bdd56f3a5e75fe0464a2bb5a
jobm <jobmwaniki18@gmail.com>
2015-10-10 14:24:29 +0300

    added urls.py to courses app

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
+from .models import Course
 # Create your views here.
+def coure_list(request):
+    courses = Course.objects.all()
+    output = ','.join(courses)
+    return HttpResponse(output)
