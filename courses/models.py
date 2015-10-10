18eb25bf15acaa948ac35ba8af2ffef8ebbc5a84
jobm <jobmwaniki18@gmail.com>
2015-10-10 14:15:59 +0300

    modified models to return a string in place of an object in __str__

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    ctrated_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    descrtiption = models.TextField()
+
+    def __str__(self):
+        return self.title
