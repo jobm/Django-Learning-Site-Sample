0bf35d3dd91f3994e5201f0b214864fe8cc59486
jobm <jobmwaniki18@gmail.com>
2015-10-11 18:07:32 +0300

    added step details view

--- courses/views.py
+++ courses/views.py
@@ -8,6 +8,10 @@ def coure_list(request):
     courses = Course.objects.all()
     return render(request, 'courses/course_list.html', { 'courses': courses})
 
-def course_detail(request,pk):
+def course_detail(request, pk):
     course = get_object_or_404(Course,pk=pk)
     return render(request,'courses/course_detail.html',{'course':course})
+
+def step_detail(request,course_pk, step_pk):
+    course = get_object_or_404(Step,course_id = course_pk,pk = step_pk)
+    return render(request,'courses/step_detail.html',{'step': step})
